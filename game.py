ca7eb6480e45877d29bc2be2cb8db70dea7bff45
Bryson Elliott <brysonelliott@Brysons-MacBook-Pro.local>
2021-10-13 21:41:24 -0600

    Game.py updated

--- game.py
+++ game.py
@@ -1,8 +1,21 @@
 import random
 
-computer_number = random.randint(1,100)
+def computer_roll():
+    computer_number = random.randint(1,100)
+    return computer_number
+    
 
-name = input('What is your name?\n')
+def main():
+    name = input('Hello! What is your name?\n')
 
-greet_player = (name, 'Can you guess the number I am thinking of?\n')
+    greet_player = name
+    print(greet_player + ' Can you guess the number that I am thinking of?\n')
 
+    user_guess = int(input('Enter a number\n'))
+
+    if user_guess == computer_roll:
+        print(greet_player + ' That is correct great job!')
+
+    else:
+        print('Not quite, try again!\n')
+main()
--- greeting.py
+++ greeting.py
@@ -1,7 +1,7 @@
 name = input('What is your name?\n')
 print(name)
 
-hello = "hello"
-greeting = hello + ''  + name
+hello = "Hello "
+greeting = hello + '' + name
 print(greeting)
 
